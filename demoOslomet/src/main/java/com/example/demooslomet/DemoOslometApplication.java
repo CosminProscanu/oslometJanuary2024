7b3172f4b87b5cc2e485cbaeeec7046da4b38973
cosmin1994p <proscanu.cosmin@gmail.com>
2024-01-26 15:03:59 +0100

    -my first commit

---
+++ demoOslomet/src/main/java/com/example/demooslomet/DemoOslometApplication.java
@@ -0,0 +1,13 @@
+package com.example.demooslomet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOslometApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOslometApplication.class, args);
+    }
+
+}
---
+++ demoOslomet/src/test/java/com/example/demooslomet/DemoOslometApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demooslomet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOslometApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
